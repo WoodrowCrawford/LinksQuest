16383a417c0a9b3089047439ec0f3bac307867b2
Woodrow Crawford <woodrowcrawford03@gmail.com>
2020-12-03 15:01:24 -0600

    Tested out a different Sprite

--- MathForGames/Game.cs
+++ MathForGames/Game.cs
@@ -185,24 +185,24 @@ namespace MathForGames
 
             //Set up console window
             Console.CursorVisible = false;
-            Console.Title = "Math For Games";
+            Console.Title = "Link's Quest";
 
             //Create a new scene for our actors to exist in
             Scene scene1 = new Scene();
             Scene scene2 = new Scene();
 
             //Create the actors to add to our scene
-            Enemy enemyHigh = new Enemy(0, 5, Color.GREEN, new Vector2(0,5), new Vector2(30, 5), '■', ConsoleColor.Green);
+            Enemy enemyHigh = new Enemy(9, 5, Color.GREEN, new Vector2(0,5), new Vector2(30, 5), '■', ConsoleColor.Green);
             Enemy enemyMid = new Enemy(10, 10, Color.GREEN, new Vector2(0, 10), new Vector2(30, 10), '■', ConsoleColor.Green);
             Enemy enemyLow = new Enemy(3, 20, Color.GREEN, new Vector2(0, 20), new Vector2(30, 20), '■', ConsoleColor.Green);
             Player player = new Player(0, 1,Color.BLUE, '#', ConsoleColor.Red);
             Goal goal = new Goal(30, 20,Color.GREEN, player, 'G', ConsoleColor.Green);
 
             //Initialize the enmies starting values
-            enemyHigh.Speed = 3;
+            enemyHigh.Speed = 1;
             enemyHigh.Target = player;
             enemyHigh.SetTranslate(new Vector2(2, 5));
-            enemyMid.Speed = 2;
+            enemyMid.Speed = 1;
             enemyMid.Target = player;
             enemyLow.Speed = 1;
             enemyLow.Target = player;
@@ -213,7 +213,7 @@ namespace MathForGames
             //Sets the player size and rotation
             player.SetTranslate(new Vector2(10, 19));
             player.SetRotation(3);
-            player.SetScale(4, 4);
+            player.SetScale(2, 2);
             player.AddChild(enemyHigh);
             //Add actors to the scenes
             scene1.AddActor(player);
@@ -250,7 +250,7 @@ namespace MathForGames
         {
             Raylib.BeginDrawing();
 
-            Raylib.ClearBackground(Color.BLACK);
+            Raylib.ClearBackground(Color.BLUE);
             Console.Clear();
             _scenes[_currentSceneIndex].Draw();
 
